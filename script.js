e6df1bad74eb148f3d0d189c20704472d46e0cfb
Caua Barros <cauagsb5@gmail.com>
2024-06-20 21:19:18 -0300

    feat: adiciona nova fonte, ajusta background e adiciona footer.

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function botaodaluz() {
-  const html = document.documentElement
-  //   if (html.classList.contains("Claro")) {
-  //    html.classList.remove("Claro")
-  //} else {
-  //  html.classList.add("Claro")
-  //  }
-  html.classList.toggle(`Claro`)
-  const img = document.querySelector(`#perfil img`)
+  const html = document.documentElement;
+  html.classList.toggle("Claro");
+
+  const img = document.querySelector("#perfil img");
   if (html.classList.contains("Claro")) {
-    img.setAttribute("src", "./assets/Avatarclarinho.png")
+    img.setAttribute("src", "./assets/Avatarclarinho.png");
   } else {
-    img.setAttribute("src", "./assets/Avatar.png")
+    img.setAttribute("src", "./assets/Avatar.png");
   }
 }
